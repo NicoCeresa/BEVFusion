d55c2b303cf5a70d910d6b95b41af99285218bab
Nico Linux <ceresanico@gmail.com>
2026-06-25 22:21:54 -0700

    moved Ibuilder enginge builder to function

--- src/cpp/infer.cpp
+++ src/cpp/infer.cpp
@@ -1,17 +1,32 @@
 #include "NvInfer.h"
+#include "NvOnnxParser.h"
 #include <stdio.h>
 #include <iostream>
 
 using namespace nvinfer1;
+using namespace nvonnxparser;
 
 class Logger : public ILogger
 {
     void log(Severity severity, const char* msg) noexcept override
     {
-        // suppress info-level messages
         if (severity <= Severity::kWARNING)
             std::cout << msg << std::endl;
     }
 } logger;
 
-IBuilder* builder = createInferBuilder(logger);
+
+
+
+IBuilder * build_engine(const char* modelFile) {
+    IBuilder* builder = createInferBuilder(logger);
+    INetworkDefinition* network = builder->createNetworkV2(0);
+    IParser* parser = createParser(*network, logger);
+    
+    parser -> parseFromFile(modelFile,
+    static_cast<int32_t>(ILogger::Severity::kWARNING));
+    for (int32_t i = 0; i < parser->getNbErrors(); ++i) {
+        std::cout << parser->getError(i)->desc() << std::endl;
+    }
+    // build engine...
+}
